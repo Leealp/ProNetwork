f54f0320ee3ccd28a584036a3f9fdfe38f1452b0
AlphaLy <lyalpha2401@gmail.com>
2019-08-07 14:38:12 -0500

    set react router, the Navbar and Landing page so they can be accessed with their corresponding buttons

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link} from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -11,8 +12,12 @@ const Landing = () => {
           other professionals
         </p>
         <div className="buttons">
-          <a href="register.html" className="btn btn-primary">Sign Up</a>
-          <a href="login.html" className="btn btn-light">Login</a>
+          <Link to="/register" className="btn btn-primary">
+            Sign Up
+            </Link>
+          <Link to="/login" className="btn btn-light">
+            Login
+          </Link>
         </div>
       </div>
     </div>
