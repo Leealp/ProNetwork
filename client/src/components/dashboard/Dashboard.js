35511d2b7a9683f9238120363af4e84a3c9e2f8d
AlphaLy <lyalpha2401@gmail.com>
2019-08-12 02:10:20 -0500

    added code to edit and update a profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
@@ -20,7 +21,9 @@ const Dashboard = ({
         <i className='fas fa-user' /> Welcome {user && user.name}   
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions/>
+        </Fragment>
         ) : (
         <Fragment>
           <p>Profile hasn't been set up yet, Please create one!</p>
