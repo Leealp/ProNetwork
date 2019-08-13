b90517e06464a0cbc39fb9df32de0feb39a2a13e
AlphaLy <lyalpha2401@gmail.com>
2019-08-13 18:17:11 -0500

    added code to get all profiles so we can later on display all users profile in the front end

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,8 @@ import {
     PROFILE_ERROR,
     CLEAR_PROFILE,
     UPDATE_PROFILE,
+    GET_PROFILES,
+    GET_REPOS
   } from '../actions/types';
 
 const initialState = {
@@ -24,6 +26,12 @@ const initialState = {
           profile: payload,
           loading: false
         };
+      case GET_PROFILES:
+        return{
+          ...state,                     
+          profile: payload,
+          loading: false
+        }
       case PROFILE_ERROR:
         return {
           ...state,
@@ -37,6 +45,12 @@ const initialState = {
               repos: [],
               loading: false
             };
+        case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      };
       default:
         return state;
     }
