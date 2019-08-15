79843c624ca60dde9669193e70223f0ee2cb8ef6
AlphaLy <lyalpha2401@gmail.com>
2019-08-14 22:44:00 -0500

    added code so education description can show on the profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -52,6 +53,23 @@ const Profile = ({
                 <h4>No experience credentials</h4>
               )}
             </div>
+            <div className='profile-edu bg-white p-2' >
+                <h2 className='text-primary'>Education</h2>
+                {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+
+
           </div>
        </Fragment>
        )}
